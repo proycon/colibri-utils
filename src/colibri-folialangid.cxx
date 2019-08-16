5cbc9cb39dbb67ea15bbd6687d7904c6412aa642
Maarten van Gompel <proycon@anaproy.nl>
2019-08-16 17:21:27 +0200

    expanding

--- src/colibri-folialangid.cxx
+++ src/colibri-folialangid.cxx
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <pair>
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
@@ -22,41 +23,62 @@ using namespace	folia;
 
 const string ISO_SET = "http://raw.github.com/proycon/folia/master/setdefinitions/iso639_3.foliaset";
 
+class Model {
+   public:
+    string lang;
+    PatternModel * model;
+    ClassDecoder * decoder;
+
+    Model(const string & lang, PatternModel * model, ClassDecoder * decoder) {
+        this->lang = lang;
+        this->model = model;
+        this->decoder = decoder;
+    }
+
+    vector<pair<string,float>> classify(const string & text) {
+        //we are going to have to do some very crude tokenisation here
+        //and then pass control over to the other classify()
+        //TODO
+    }
+
+    vector<pair<string,float>> classify(const vector<string> & text) {
+        vector<pair<string,float>> results;
+        //this is where the magic happens
+        //TODO: sort result by confidence before returning
+        return results;
+    }
+};
+
 void usage( const string& name ){
     cerr << "Usage: " << name << " [options] file" << endl;
     cerr << "Description: Language identification on a FoLiA document against various colibri models" << endl;
     cerr << "Options:" << endl;
     cerr << "--lang=<code>\t use 'code' for unindentified text." << endl;
     cerr << "--tags=t1,t2,..\t examine text in all <t1>, <t2> ...  nodes. (default is to use the <p> nodes)." << endl;
+    cerr << "--all\t\t assign ALL detected languages to the result. (default is to assign the most probable)." << endl;
+    cerr << "\t--lowercase\t Lowercase all text (make sure the models are trained like this too if you use this!)" << endl;
     cerr << "\t--models\t This list consists of languagecode,modelfile,classfile tuples, the tuples are semicolon separated" << endl;
     cerr << "\t--class\t class (default: current)" << endl;
     cerr << "\t-h or --help\t this message " << endl;
     cerr << "\t-V or --version\t show version " << endl;
 }
 
-void setlang( FoliaElement* e, const string& lan ){
+void setlang( FoliaElement* e, const string& langcode ){
     // append a LangAnnotation child of class 'lan'
     KWargs args;
-    args["class"] = lan;
+    args["class"] = langcode;
     args["set"] = ISO_SET;
     LangAnnotation *node = new LangAnnotation( args, e->doc() );
     e->replace( node );
 }
 
-void addLang( const TextContent *t, const vector<string>& lv, bool doAll ) {
-    //
-    // we expect something like [dutch][french]
-    //
-    string val;
-    for ( const auto& l : lv ){
-        val += l;
-        if ( !doAll ) break;
-        if ( &l != &lv.back() ){
-          val += "|";
+void addLang( const TextContent *t, const vector<std::pair<string,float>>& results, bool doAll ) {
+    //assume results is sorted!
+    for ( const auto& l : results ){
+        setlang( t->parent(), val );
+        if (!doAll) {
+            break;
         }
-    }
-    if ( !val.empty() ){
-      setlang( t->parent(), val );
     }
 }
 
@@ -84,100 +106,88 @@ vector<FoliaElement*> gather_nodes( Document *doc, const string& docName, const 
   return result;
 }
 
-void processFile( const Vector<PatternModel>& tc,
+void processFile( const Vector<Models>& models,
 		 const string& outDir, const string& docName,
 		 const string& default_lang,
 		 set<string> tags,
 		 bool doAll,
 		 const string& cls,
-		 const string& command )
+		 const string& command,
+		 bool lowercase)
 
-  cout << "process " << docName << endl;
-  Document *doc = 0;
-  try {
-    doc = new Document( "file='" + docName + "'" );
-  }
-  catch (const exception& e){
-#pragma omp critical (logging)
-    {
-      cerr << "no document: " << e.what() << endl;
-    }
-    return;
-  }
-  doc->set_metadata( "language", default_lang );
-  processor *proc = add_provenance( *doc, "FoLiA-langcat", command );
-  if ( !doc->declared(  AnnotationType::LANG, ISO_SET ) ){
-    KWargs args;
-    args["processor"] = proc->id();
-    doc->declare( AnnotationType::LANG, ISO_SET, args );
-  }
-  string outName;
-  if ( !outDir.empty() ){
-    outName = outDir + "/";
-  }
-  string::size_type pos = docName.rfind("/");
-  if ( pos != string::npos ){
-    outName += docName.substr( pos+1);
-  }
-  else {
-    outName += docName;
-  }
-  string::size_type xml_pos = outName.find(".folia.xml");
-  if ( xml_pos == string::npos ){
-    xml_pos = outName.find(".xml");
-  }
-  outName.insert( xml_pos, ".lang" );
-  if ( !TiCC::createPath( outName ) ){
-#pragma omp critical (logging)
-    {
-      cerr << "unable to open output file " << outName << endl;
-      cerr << "does the outputdir exist? And is it writabe?" << endl;
-    }
-    exit( EXIT_FAILURE);
-  }
-  vector<FoliaElement*> nodes = gather_nodes( doc, docName, tags );
-  for ( const auto& node : nodes ){
-    const TextContent *t = 0;
-    string id = "NO ID";
+    cout << "process " << docName << endl;
+    Document *doc = 0;
     try {
-      id = node->id();
-      t = node->text_content(cls);
+        doc = new Document( "file='" + docName + "'" );
+    } catch (const exception& e){
+        cerr << "no document: " << e.what() << endl;
+        return;
     }
-    catch (...){
+    doc->set_metadata( "language", default_lang );
+    processor *proc = add_provenance( *doc, "colibri-folialangid", command );
+    if ( !doc->declared(  AnnotationType::LANG, ISO_SET ) ){
+        KWargs args;
+        args["processor"] = proc->id();
+        doc->declare( AnnotationType::LANG, ISO_SET, args );
     }
-    if ( t ){
-      string para = t->str();
-      para = TiCC::trim( para );
-      if ( para.empty() ){
-	if ( verbose )
-#pragma omp critical (logging)
-	  {
-	    cerr << "WARNING: no textcontent " << id << endl;
-	  }
-      }
-      else {
-        TiCC::to_lower( para );
-        vector<string> lv = tc.get_languages( para );
-        addLang( t, lv, doAll );
-      }
+    string outName;
+    if ( !outDir.empty() ){
+        outName = outDir + "/";
     }
-    else if ( verbose ){
-#pragma omp critical (logging)
-      {
-	cerr << "WARNING: no textcontent " << id << endl;
-      }
+    string::size_type pos = docName.rfind("/");
+    if ( pos != string::npos ){
+       outName += docName.substr( pos+1);
+    } else {
+       outName += docName;
     }
-  }
-  doc->save(outName);
-  delete doc;
+    string::size_type xml_pos = outName.find(".folia.xml");
+    if ( xml_pos == string::npos ){
+        xml_pos = outName.find(".xml");
+    }
+
+    outName.insert( xml_pos, ".lang" );
+    if ( !TiCC::createPath( outName ) ){
+       cerr << "unable to open output file " << outName << endl;
+       cerr << "does the outputdir exist? And is it writabe?" << endl;
+       exit( EXIT_FAILURE);
+    }
+
+    vector<FoliaElement*> nodes = gather_nodes( doc, docName, tags );
+    for ( const auto& node : nodes ){
+       const TextContent *t = 0;
+       string id = "NO ID";
+       try {
+           id = node->id();
+           t = node->text_content(cls);
+       } catch (...){ }
+
+       if ( t ){
+         string text = t->str();
+         text = TiCC::trim( text );
+         if ( text.empty() ){
+             if ( verbose ) cerr << "WARNING: no textcontent " << id << endl;
+         } else {
+             if (lowercase) {
+                 TiCC::to_lower(text);
+             }
+             for (const auto& model: models) {
+                 vector<std::pair<string,float>> = model.classify(text)
+                 addLang( t, lv, doAll );
+             }
+         }
+       } else if ( verbose ) {
+           cerr << "WARNING: no textcontent " << id << endl;
+       }
+    }
+    doc->save(outName);
+    delete doc;
 }
 
 int main( int argc, const char *argv[] ) {
-    TiCC::CL_Options opts( "vVhO:", "models,classfile,class,version,help,lang:,tags:,class:" );
+    TiCC::CL_Options opts( "vVhO:", "models,classfile,class,version,help,lang:,tags:,class:,lowercase" );
     try {
         opts.init( argc, argv );
-    }
-    catch( TiCC::OptionError& e ){
+    } catch( TiCC::OptionError& e ){
         cerr << e.what() << endl;
         usage(argv[0]);
         exit( EXIT_FAILURE );
@@ -197,8 +207,10 @@ int main( int argc, const char *argv[] ) {
     string command = "colibri-folialangid " + opts.toString();
 
 
+    const bool lowercase = opts.extract("lowercase");
+    const bool doAll = opts.extract( "all" );
 
-
+    string lang;
     opts.extract( "lang", lang );
     opts.extract( "class", inputclass );
     opts.extract( 'O', outDir );
@@ -208,7 +220,7 @@ int main( int argc, const char *argv[] ) {
     opts.extract( "tags", tagsstring );
     if ( !tagsstring.empty() ){
         vector<string> parts = TiCC::split_at( tagsstring, "," );
-        for( const auto& t : parts ){
+        for( const auto& t : parts {
           tags.insert( t );
         }
     }
@@ -248,7 +260,7 @@ int main( int argc, const char *argv[] ) {
     }
     for ( size_t fn=0; fn < toDo; ++fn ){
         string docName = fileNames[fn];
-        processFile( TC, outDir, docName, lang, tags, cls, command );
+        processFile( models, outDir, docName, lang, tags, doAll, inputclass, command, lowercase );
     }
     return EXIT_SUCCESS;
 }
